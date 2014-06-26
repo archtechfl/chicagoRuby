65962fb41e2fb2a19174a87672ab0cb19a6e8f86
Jeremy Moore <jeremy@moremooredesign.com>
2014-06-25 20:27:17 -0400

    stations list displayed

--- app/controllers/visualize_controller.rb
+++ app/controllers/visualize_controller.rb
@@ -1,7 +1,15 @@
 class VisualizeController < ApplicationController
   def index
+   require 'open-uri'
    stations_json = open("http://data.cityofchicago.org/resource/8pix-ypme.json").read
-   stations_list = JSON.parse(stations_json)
-   @visualize = stations_list
+   raw_stations = JSON.parse(stations_json)
+  
+   @visualize = cooking(raw_stations)
   end
+end
+
+
+def cooking(raw_stations)
+  cooked_stations_list = raw_stations
+  return cooked_stations_list
 end
