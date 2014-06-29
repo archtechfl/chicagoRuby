feaf438dd08917c6257243b5348212a398d287c7
Erich Welz <erichwelz@gmail.com>
2014-06-29 14:56:57 -0400

    simple filter of directions

--- app/controllers/visualize_controller.rb
+++ app/controllers/visualize_controller.rb
@@ -4,37 +4,19 @@ class VisualizeController < ApplicationController
    stations_json = open("http://data.cityofchicago.org/resource/8pix-ypme.json").read
    raw_stations = JSON.parse(stations_json)
   
-   @visualize = cooking(raw_stations)
+   @visualize = elim_duplicates(raw_stations)
   end
 end
 
 
-def cooking(raw_stations)
-  raw_stations_list = raw_stations
-
-  #array = raw_stations_list.keys
-  #length = arrays.size
-
-  #puts length
-
-  blacklist = Array.new
+def elim_duplicates(raw_stations)
   non_dupe_list = Array.new
 
-  raw_stations_list.each do |burger|
-    name_check = burger["station_descriptive_name"]
-
-
-   # check if current item matches any item in names array 
-    # if name_check == 'any item in blacklist' 
-    #  rejection 
-    #  else 
-    #   push name to blacklist 
-     blacklist.push(name_check)
-     non_dupe_list.push(burger)
-    #   push non_dupe_list
-    # end
-
+  raw_stations.each do |input|
+ 
+    if input['direction_id'] == "E" || input['direction_id'] == "N"
+     non_dupe_list.push(input)
+    end
   end
-
   return non_dupe_list
 end
